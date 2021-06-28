c124b009ab1c899996d866fb3a04ec8b5cca1fed
werwolf2303 <66752411+werwolf2303@users.noreply.github.com>
2021-06-28 22:45:58 +0200

    Add files via upload

---
+++ splitted/100MB.bat
@@ -0,0 +1,1 @@
+copy /b "100MB.bin.001" + "100MB.bin.002" + "100MB.bin.003" + "100MB.bin.004" + "100MB.bin.005" + "100MB.bin.006" + "100MB.bin.007" + "100MB.bin.008" + "100MB.bin.009" + "100MB.bin.010"  "100MB.bin"
